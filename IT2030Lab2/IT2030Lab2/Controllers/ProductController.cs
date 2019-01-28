8ed639d9f87ca600c6da3ff7835f11cceac9a3b8
William Leavitt <dragoneye13@cox.net>
2019-01-27 21:59:11 -0500

    Did Lab 02

---
+++ IT2030Lab2/IT2030Lab2/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IT2030Lab2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public string Index()
+        {
+            return "Product/Index is displayed";
+        }
+
+        public string Browse() {
+            return "Browse Displayed";
+        }
+
+        public string Details(int id) {
+            return "Details displayed for ID = " + id;
+        }
+
+        public string Location(int zip) {
+            return "Location displayed for zip = " + zip;
+        }
+    }
+}
