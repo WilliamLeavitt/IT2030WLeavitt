bea007e174292df9a6f5229f8d80d5a940c8e03e
William Leavitt <dragoneye13@cox.net>
2019-02-17 11:21:56 -0500

    Lab 05 Changes to Enrollment Application/Music Store

--- EnrollmentApplication/EnrollmentApplication/Models/Enrollment.cs
+++ EnrollmentApplication/EnrollmentApplication/Models/Enrollment.cs
@@ -13,5 +13,9 @@ namespace EnrollmentApplication.Models
         public virtual string grade { get; set; }
         public virtual Student student { get; set; }
         public virtual Course course { get; set; }
+        public virtual bool IsActive { get; set; }
+        public virtual string AssignedCampus { get; set; }
+        public virtual string EnrollmentSemester { get; set; }
+        public virtual int EnrollmentYear { get; set; }
     }
 }
