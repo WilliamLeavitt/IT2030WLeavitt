83ab6903b10242deeb5e078ecc72e6aeb575e239
William Leavitt <dragoneye13@cox.net>
2019-03-31 11:35:52 -0400

    Lab 10 Completed

---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStoreApplication.Models;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        private MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        // GET: Store
+        [HttpGet]
+        public ActionResult Index(int? id)
+        {
+            //get albums with matching genre
+            var results = db.Albums.Where(a => a.GenreId == id);
+            return View(results.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Browse()
+        {
+            var albums = db.Genres;
+            return View(albums.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            var album = db.Albums.Find(id);
+            return View(album);
+        }
+    }
+}
